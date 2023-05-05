68faf838a9b13479b34d7c0945fafe850dca807d
fabio <fabio_batoni@hotmail.com>
2023-05-05 18:14:16 -0300

    create session routes

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -3,11 +3,14 @@ const { Router } = require("express");
 const usersRoutes = require("./users.routes");
 const movieNotesRoutes = require("./movieNotes.routes");
 const movieTagsRoutes = require("./movieTags.routes");
+const sessionRouter = require("./sessions.routes");
 
 const routes = Router();
 
 routes.use("/users", usersRoutes);
+routes.use("/sessions", sessionRouter);
 routes.use("/movie_notes", movieNotesRoutes);
 routes.use("/movie_tags", movieTagsRoutes);
+
 
 module.exports = routes;
