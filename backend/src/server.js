f03077aafbef42e4cd1568e6f8e715fd1c4eb2e7
fabio <fabio_batoni@hotmail.com>
2023-05-13 16:57:03 -0300

    get file upload

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,9 +6,13 @@ const AppError = require("./utils/AppError");
 
 const routes = require("./routes");
 
+const uploadConfig = require("./configs/upload");
+
 const app = express();
 
 app.use(express.json());
+
+app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER))
 
 app.use(routes);
 
